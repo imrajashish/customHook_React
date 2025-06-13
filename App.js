dbebe8ddea109c393af88b09873d6345c6612c8a
Ashish Kumar Raj <61040769+imrajashish@users.noreply.github.com>
2025-06-13 23:37:43 +0530

    Create App.js
    Main file of the folder

---
+++ App.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+import RestorentMenu from "./RestorentMenu";
+import Carousels from "./Caresole";
+import Accordion from "./Accordion";
+
+export default function App() {
+  const items = [
+    {
+      title: "JavaScript Basics",
+      content: "Learn variables, functions, and loops in JavaScript.",
+    },
+    {
+      title: "React.js Overview",
+      content: "Understand components, state, and props in React.",
+    },
+    {
+      title: "Node.js",
+      content: "Basics of server-side development with Node.js.",
+    },
+    {
+      title: "Full-Stack Development",
+      content: "Build full-stack apps with React and Node.js.",
+    },
+  ];
+
+  return (
+    <div className="App">
+      {/* <RestorentMenu/> */}
+      {/* <Carousels /> */}
+      <Accordion items={items} />
+    </div>
+  );
+}
